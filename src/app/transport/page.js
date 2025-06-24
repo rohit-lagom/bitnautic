3bd6f5ea17d0ae84deffdb9b0233db9c696ff5c2
rohit-lagom <rohit@lagomchain.com>
2025-06-24 13:53:57 +0530

    feat (transport page): added basic transport page

---
+++ src/app/transport/page.js
@@ -0,0 +1,8 @@
+export default function Transport() {
+  return (
+    <section className="py-16 px-4">
+      <h1 className="text-4xl font-bold mb-6">Transport </h1>
+     
+    </section>
+  );
+}

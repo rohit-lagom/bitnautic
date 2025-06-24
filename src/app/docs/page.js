496bec126c049f86170f4d02292a230b308f4322
rohit-lagom <rohit@lagomchain.com>
2025-06-24 13:52:32 +0530

    feat (enterprise page): added basic enterprise page

--- src/app/docs/page.js
+++ src/app/docs/page.js
@@ -1,7 +1,7 @@
 export default function Docs() {
   return (
     <section className="py-16 px-4">
-      <h1 className="text-4xl font-bold mb-6">About Us</h1>
+      <h1 className="text-4xl font-bold mb-6">Docs </h1>
      
     </section>
   );
---
+++ src/app/enterprise/page.js
@@ -0,0 +1,8 @@
+export default function Enterprise() {
+  return (
+    <section className="py-16 px-4">
+      <h1 className="text-4xl font-bold mb-6">Enterprise </h1>
+     
+    </section>
+  );
+}
